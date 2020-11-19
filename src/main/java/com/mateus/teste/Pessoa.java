0c826587e861da7e5e2d071ca34b0a228fc1dd16
Francisco Mateus Santos Pinto <ms825957@gmail.com>
2020-11-19 19:57:06 -0200

    primeiro commit

---
+++ src/main/java/com/mateus/teste/Pessoa.java
@@ -0,0 +1,30 @@
+
+package com.mateus.teste;
+
+
+public class Pessoa {
+    private String nome;
+    private int cpf;
+    
+    private Pessoa(String nome,int cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    
+}
